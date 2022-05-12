f6fa52d68b29d4cbd5c4fa44b9d18840a793b037
Fabio Moro <fabio.moro2000@gmail.com>
2022-05-12 16:05:00 +0200

    correzioni

--- main.js
+++ main.js
@@ -6,32 +6,37 @@ for(let i=0; i < 5; i++) {
     container.append(document.createElement("div").innerText = numbers[i]+" ");
 }
 
+let guessed = [];
+let notGuessed = [];
 setTimeout(clear, 5000);
-let string1 = "Numeri indovinati: ";
-let string2= "Numeri non indovinati: ";
+
 
 function clear() {
     document.querySelector(".mycontainer").innerHTML="";
-    setTimeout(myFunction, 1000);
-
+    setTimeout(myFunction, 500);
 }
 
 function myFunction() {
+    let number;
     for(let i=0; i<5; i++) {
-        let number = Number(prompt("inserisci il numero"));
+
+        do {
+             number = Number(prompt("inserisci il numero"));
+        }
+        while(guessed.includes(number))
+        
         if (numbers.includes(number)) {
-            string1 = string1 + number + " ";
+            guessed.push(number);
         } else {
-            string2 = string2 + numbers[i] + " ";
+            notGuessed.push(numbers[i]);
         }
     }
 
-    let element1 = document.createElement("div");
-    element1.innerText = string1
+    let element = document.createElement("div").innerText = "Numeri esatti: "+guessed;
+    container.append(element);
+    container.append(document.createElement("br"));
+    element = document.createElement("div").innerText = "Numeri che non hai ricordato: "+notGuessed;
+    container.append(element);
 
-    let element2 = document.createElement("div");
-    element2.innerText = string2
-    
-    container.append(element1, element2);
 }
     
